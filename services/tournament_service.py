a63b7502b8808bc7efd9c839fa3515a9dcad8ff6
Carnage098 <NOREPLY>
2026-07-07 14:44:55 +0200

    Create tournament_service.py

---
+++ services/tournament_service.py
@@ -0,0 +1,21 @@
+class TournamentService:
+
+    async def create(...)
+
+    async def delete(...)
+
+    async def get(...)
+
+    async def get_by_code(...)
+
+    async def list(...)
+
+    async def open_registration(...)
+
+    async def close_registration(...)
+
+    async def start(...)
+
+    async def finish(...)
+
+    async def exists(...)
