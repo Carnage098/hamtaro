5a4a7439166b12ab899d08776bce7308768c0224
Carnage098 <NOREPLY>
2026-07-07 14:37:16 +0200

    Update registration.py

--- models/registration.py
+++ models/registration.py
@@ -1,1 +1,17 @@
 
+from dataclasses import dataclass
+from typing import Optional
+
+
+@dataclass
+class Registration:
+
+    id: Optional[int]
+
+    tournament_id: int
+
+    discord_id: str
+
+    username: str
+
+    deck: Optional[str]
