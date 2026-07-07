fe3670f0be00a60ea11c4f21d80b33cb37da4751
Carnage098 <polairebleu7@gmail.com>
2026-07-07 16:07:14 +0200

    Update enums.py

--- models/enums.py
+++ models/enums.py
@@ -1,1 +1,27 @@
+from __future__ import annotations
 
+from enum import Enum
+
+
+class TournamentStatus(str, Enum):
+    REGISTRATION = "registration"
+    CHECK_IN = "check_in"
+    RUNNING = "running"
+    FINISHED = "finished"
+    CANCELLED = "cancelled"
+
+
+class MatchStatus(str, Enum):
+    WAITING = "waiting"
+    PLAYING = "playing"
+    REPORTED = "reported"
+    VALIDATED = "validated"
+    COMPLETED = "completed"
+    CANCELLED = "cancelled"
+
+
+class ResultType(str, Enum):
+    PLAYER1 = "player1"
+    PLAYER2 = "player2"
+    DRAW = "draw"
+    BYE = "bye"
