50e06ef2882283cc1b5545d4d0263409e49464b9
Carnage098 <polairebleu7@gmail.com>
2026-07-10 15:05:49 +0200

    Create __init__.py

---
+++ graphics/__init__.py
@@ -0,0 +1,14 @@
+"""
+Ressources graphiques de Hamtaro.
+
+Ce package contient :
+- le thème officiel du bracket ;
+- les chemins vers les ressources visuelles ;
+- les futures variantes graphiques.
+"""
+
+from graphics.theme import HamtaroBracketTheme
+
+__all__ = [
+    "HamtaroBracketTheme",
+]
